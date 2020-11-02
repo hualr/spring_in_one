27b850b9b7472ab6ce78f7da53523a996fa558f8
zzongqi <3031720178@qq.com>
2020-11-02 20:04:14 +0800

    环境变量初始化-方法3 在properties中添加初始化配置属性 这里的优先级是最高 order越小越先执行

---
+++ src/main/java/com/example/spring_in_one/initializer/ThirdInitializer.java
@@ -0,0 +1,22 @@
+package com.example.spring_in_one.initializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+@Order(3)
+public class ThirdInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        Map<String, Object> map = new HashMap<>();
+        map.put("key3", "value3");
+        MapPropertySource mapPropertySource = new MapPropertySource("thirdInitializer", map);
+        environment.getPropertySources().addLast(mapPropertySource);
+        System.out.println("run thirdInitializer");
+    }
+}
