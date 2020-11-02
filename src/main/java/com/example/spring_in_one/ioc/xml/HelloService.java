35e22ea5daa027a62800e797b02234aa5a00b2a1
zzongqi <3031720178@qq.com>
2020-11-02 21:43:30 +0800

    bean配置: IOCXML-无参构造

---
+++ src/main/java/com/example/spring_in_one/ioc/xml/HelloService.java
@@ -0,0 +1,22 @@
+package com.example.spring_in_one.ioc.xml;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String hello() {
+        return student.toString();
+    }
+}
--- src/test/java/com/example/spring_in_one/SpringInOneApplicationTests.java
+++ src/test/java/com/example/spring_in_one/SpringInOneApplicationTests.java
@@ -1,23 +1,26 @@
 package com.example.spring_in_one;
 
-import com.example.spring_in_one.event.WeatherRunListener;
+import com.example.spring_in_one.ioc.xml.HelloService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(locations = "classpath:ioc/demo.xml")
 class SpringInOneApplicationTests {
 
+
     @Autowired
-    private WeatherRunListener weatherRunListener;
+    private HelloService helloService;
+
     @Test
     void contextLoads() {
     }
 
     @Test
-    public void test(){
-        weatherRunListener.rain();
-        weatherRunListener.snow();
+    public void test() {
+        System.out.println(helloService.hello());
     }
 
 }
